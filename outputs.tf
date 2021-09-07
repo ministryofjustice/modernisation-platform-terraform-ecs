5292cbdc18f1bbcd179c4f4e9fb0f4e8ec97208c
zuriguardiola <zuri.guardiola@digital.justice.gov.uk>
2021-09-07 22:39:52 +0100

    Add output description

--- outputs.tf
+++ outputs.tf
@@ -4,5 +4,6 @@ output "cluster_ec2_security_group_id" {
 }
 
 output "current_task_definition" {
+  description = "Displays task definition information and version being used"
   value = data.aws_ecs_task_definition.task_definition
 }
