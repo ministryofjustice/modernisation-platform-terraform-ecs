79ad32e85bb20bfa55baa097d02f97a9c02f5eca
Edward Proctor <edward.proctor@digital.justice.gov.uk>
2022-10-31 14:40:59 +0000

    Renamed outputs to specifcy arns, as well as better name and description for all.

--- outputs.tf
+++ outputs.tf
@@ -23,17 +23,17 @@ output "ecs_task_execution_policy" {
   value       = data.aws_iam_policy_document.ecs_task_execution_role
 }
 
-output "app_scale_up_policy"{
-  description = "App scale up policy output"
+output "app_scale_up_policy_arn"{
+  description = "ARN for app autoscaling - scaling policy up"
   value = aws_appautoscaling_policy.scaling_policy_up.arn
 }
 
-output "app_scale_down_policy"{
-  description = "App scale down policy output"
+output "app_scale_down_policy_arn"{
+  description = "ARN for app autoscaling - scaling policy down"
   value = aws_appautoscaling_policy.scaling_policy_down.arn
 }
 
-output "app_autoscale_name"{
-  description = "Autoscale Name"
-  value = aws_autoscaling_group.cluster-scaling-group.arn
+output "ec2_autoscaling_name"{
+  description = "Autoscaling group name"
+  value = aws_autoscaling_group.cluster-scaling-group
 }
