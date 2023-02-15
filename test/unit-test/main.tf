e2501db659a069caa849fd4d417c82d8e53eb35a
George Taylor <george.taylor@digital.justice.gov.uk>
2023-02-15 16:13:48 +0000

    replace deprecated template provider with template_file function

--- test/unit-test/main.tf
+++ test/unit-test/main.tf
@@ -9,9 +9,9 @@ module "ecs" {
   environment             = local.environment
   ami_image_id            = data.aws_ami.latest.image_id
   instance_type           = local.app_data.accounts[local.environment].instance_type
-  user_data               = base64encode(data.template_file.launch-template.rendered)
+  user_data                = base64encode(templatefile("templates/user-data.txt", { cluster_name = local.application_name }))
   key_name                = local.app_data.accounts[local.environment].key_name
-  task_definition         = data.template_file.task_definition.rendered
+  task_definition         = templatefile("templates/task_definition.json")
   ec2_desired_capacity    = local.app_data.accounts[local.environment].ec2_desired_capacity
   ec2_max_size            = local.app_data.accounts[local.environment].ec2_max_size
   ec2_min_size            = local.app_data.accounts[local.environment].ec2_min_size
@@ -59,17 +59,6 @@ data "aws_route53_zone" "network-services" {
 
   name         = "modernisation-platform.service.justice.gov.uk."
   private_zone = false
-}
-
-data "template_file" "task_definition" {
-  template = file("templates/task_definition.json")
-}
-
-data "template_file" "launch-template" {
-  template = file("templates/user-data.txt")
-  vars = {
-    cluster_name = local.application_name
-  }
 }
 
 data "aws_route53_zone" "external" {
