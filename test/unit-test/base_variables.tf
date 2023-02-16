bbd82541e2ab0214476c3ecb5f2640a7eca503c7
George Taylor <george.taylor@digital.justice.gov.uk>
2023-02-16 11:30:02 +0000

    check gh run var in pipeline

--- test/unit-test/base_variables.tf
+++ test/unit-test/base_variables.tf
@@ -8,3 +8,7 @@ variable "GH_RUN" {
   type        = string
   description = "The GitHub run id/attempt which is used to uniquely identify resources against their workflow run"
 }
+
+output "test" {
+  value = var.GH_RUN
+}
