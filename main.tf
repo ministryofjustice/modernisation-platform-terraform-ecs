811f16ed7e193ba413ce07ced54d3ea9857a0e19
George Taylor <george.taylor@digital.justice.gov.uk>
2023-02-17 10:55:29 +0000

    move data sources to separate file

--- main.tf
+++ main.tf
@@ -1,22 +1,3 @@
-data "aws_ecs_task_definition" "task_definition" {
-  task_definition = "${var.app_name}-task-definition"
-  depends_on      = [aws_ecs_task_definition.windows_ecs_task_definition, aws_ecs_task_definition.linux_ecs_task_definition]
-}
-
-data "aws_subnets" "shared-private" {
-  filter {
-    name   = "vpc-id"
-    values = [var.vpc_id]
-  }
-  tags = {
-    Name = "${var.subnet_set_name}-private*"
-  }
-}
-
-data "aws_lb_target_group" "target_group" {
-  name = var.lb_tg_name
-}
-
 resource "aws_autoscaling_group" "cluster-scaling-group" {
   vpc_zone_identifier = sort(data.aws_subnets.shared-private.ids)
   desired_capacity    = var.ec2_desired_capacity
